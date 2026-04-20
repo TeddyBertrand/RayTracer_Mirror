ba00068158c88bb7a0e4ebae5030e0f994e2e954
Bertrand Teddy <teddy.bertrand@epitech.eu>
2026-04-20 11:10:28 +0200

    test(pipeline): shouldnt work

--- src/main.cpp
+++ src/main.cpp
@@ -3,4 +3,4 @@
 int main() {
     std::cout << "Hi!" << std::endl;
     return 0;
-}
+Bah ça compile pas tout ça non ? C'est normal, c'est juste un test pour voir si le pipeline fonctionne. On va ajouter du code plus tard.
