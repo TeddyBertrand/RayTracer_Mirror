ec6886b5a480bf553bfa0775d8a5bce659a175c5
maeva <maeva.arroniz@epitech.eu>
2026-04-21 11:42:01 +0200

    documentation(sphere): add inline doxygen documentation for .hpp

--- src/primitives/sphere/Sphere.hpp
+++ src/primitives/sphere/Sphere.hpp
@@ -8,9 +8,24 @@ namespace Raytracer
     class Sphere : public virtual IPrimitive
     {
         public:
+            /**
+             * @brief Construct a new Sphere object with params
+             * 
+             * @param center 
+             * @param radius 
+             */
             Sphere(Point3D center, double radius) : _center(center), _radius(radius) {}
-            Sphere() = default;
 
+            /**
+             * @brief Hit function
+             * 
+             * check if a ray between an interval t0 and t1 has hit the sphere or not 
+             * @param r 
+             * @param ray_t 
+             * @param rec 
+             * @return true 
+             * @return false 
+             */
             bool hit(const Ray& r, Interval ray_t, HitRecord& rec) const;
 
         private:
