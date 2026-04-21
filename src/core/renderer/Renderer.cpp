6eb488c5acb38f0f4bf3aaded1b8cf8f15a61344
Saitamadupauvre <lembrez.alban@gmail.com>
2026-04-21 17:59:55 +0200

    feat(Renderer): refactor rendering to use Scene class for managing primitives

--- src/core/renderer/Renderer.cpp
+++ src/core/renderer/Renderer.cpp
@@ -3,7 +3,7 @@
 namespace Raytracer
 {
 
-void Renderer::render(const ICamera& camera, const IPrimitive& world, FrameBuffer& buffer)
+void Renderer::render(const ICamera& camera, const Scene& scene, FrameBuffer& buffer)
 {
     int width = camera.getWidth();
     int height = camera.getHeight();
@@ -18,25 +18,24 @@ void Renderer::render(const ICamera& camera, const IPrimitive& world, FrameBuffe
             double v = 1.0 - static_cast<double>(y) / (height - 1.0);
 
             Ray r = camera.get_ray(u, v);
-            pixel_color += ray_color(r, world, 50);
+            pixel_color += ray_color(r, scene, 50);
             buffer[y * width + x] = pixel_color;
         }
     }
 }
 
-Color Renderer::ray_color(const Ray& r, const IPrimitive& world, int depth)
+Color Renderer::ray_color(const Ray& r, const Scene& scene, int depth)
 {
     if (depth <= 0)
         return Color(0, 0, 0);
 
     HitRecord rec;
-    if (world.hit(r, Interval(0.001, Interval::universe.max), rec)) {
+    if (scene.getWorld().hit(r, Interval(0.001, std::numeric_limits<double>::infinity()), rec)) {
+        
         return 0.5 * (Color(rec.normal.x, rec.normal.y, rec.normal.z) + Color(1, 1, 1));
     }
 
-    Vector3D unit_direction = r.direction().normalized();
-    double t = 0.5 * (unit_direction.y + 1.0);
-    return (1.0 - t) * Color(1, 1, 1) + t * Color(0.5, 0.7, 1.0);
+    return scene.getBackground(r);
 }
 
 } // namespace Raytracer
