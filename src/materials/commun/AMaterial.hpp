d4af410004113ce064e614821dd15c80ed9d578c
Saitamadupauvre <lembrez.alban@gmail.com>
2026-04-23 09:30:01 +0200

    feat(material): add abstract material class AMaterial

---
+++ src/materials/commun/AMaterial.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "components/IMaterial.hpp"
+
+namespace Raytracer
+{
+
+class AMaterial : public IMaterial
+{
+protected:
+    AMaterial() = default;
+};
+
+} // namespace Raytracer
