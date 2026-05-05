da18b9099e89974567f744d70862e4ebc0bccdc9
maeva <maeva.arroniz@epitech.eu>
2026-05-05 16:01:38 +0200

    feat(materials): implement MetalBSDF class with scatter method and evaluate function

---
+++ src/materials/commun/bsdf/metal/MetalBSDF.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "components/ITexture.hpp"
+#include "materials/commun/bsdf/ABSDF.hpp"
+#include <memory>
+
+namespace Raytracer {
+
+class MetalBSDF : public ABSDF {
+public:
+    explicit MetalBSDF(std::shared_ptr<ITexture> tex, double fuzz = 0.0)
+        : _albedo_texture(tex),
+          _fuzz(fuzz < 0.0 ? 0.0 : (fuzz > 1.0 ? 1.0 : fuzz)) {}
+
+    /**
+     * @brief Sample bounce 
+     * 
+     * @param r_in 
+     * @param hit 
+     * @param attenuation 
+     * @param scattered 
+     * @return true 
+     * @return false 
+     */
+    bool scatter(const Ray& r_in,
+                 const HitRecord& hit,
+                 Color& attenuation,
+                 Ray& scattered) const override;
+
+    /**
+     * @brief Evaluate color from hit
+     * 
+     * @param light_dir 
+     * @param view_dir 
+     * @param hit 
+     * @return Color 
+     */
+    Color evaluate(const Vector3D& light_dir,
+                   const Vector3D& view_dir,
+                   const HitRecord& hit) const override {
+        return Color(0, 0, 0); 
+    }
+
+private:
+    std::shared_ptr<ITexture> _albedo_texture;
+    double _fuzz;
+};
+
+};
