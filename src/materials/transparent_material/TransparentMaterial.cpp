e5c22c9e3e44a069f98a1e33f3ce7c18acee3167
maeva <maeva.arroniz@epitech.eu>
2026-05-12 17:17:37 +0200

    feat(transparent material): add support for normal mapping in TransparentMaterial

--- src/materials/transparent_material/TransparentMaterial.cpp
+++ src/materials/transparent_material/TransparentMaterial.cpp
@@ -1,4 +1,5 @@
 #include "TransparentMaterial.hpp"
+#include "materials/commun/bsdf/normal_mapping/NormalMappingBSDF.hpp"
 #include "materials/commun/texture/Texture.hpp"
 #include "parser/ISettings.hpp"
 
@@ -12,7 +13,15 @@ IMaterial* createPlugin(const ISetting& settings) {
     double ref = settings.getFloat("ref", 0.0);
     std::shared_ptr<ITexture> tex = Texture::fromSetting(settings, "color");
 
-    return new TransparentMaterial(tex, ref);
+    std::shared_ptr<IBSDF> bsdf;
+    if (settings.exists("normal")) {
+        auto phong_bsdf = std::make_shared<TransparentBSDF>(tex, ref);
+        std::shared_ptr<ITexture> normal_texture = Texture::fromSetting(settings, "normal");
+        double normal_strength = settings.getFloat("normal_strength", 1.0);
+        bsdf = std::make_shared<NormalMappingBSDF>(phong_bsdf, normal_texture, normal_strength);
+    }
+
+    return new TransparentMaterial(tex, ref, bsdf);
 }
 }
 
