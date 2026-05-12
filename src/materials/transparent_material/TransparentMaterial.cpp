a554a3e3b8f57af9ebcb831480eef0eb787d3802
maeva <maeva.arroniz@epitech.eu>
2026-05-12 17:48:12 +0200

    feat(transparent material): implement constructor logic for TransparentMaterial in cpp

--- src/materials/transparent_material/TransparentMaterial.cpp
+++ src/materials/transparent_material/TransparentMaterial.cpp
@@ -25,4 +25,15 @@ IMaterial* createPlugin(const ISetting& settings) {
 }
 }
 
+TransparentMaterial::TransparentMaterial(std::shared_ptr<ITexture> tex,
+                    double ref,
+                    std::shared_ptr<IBSDF> custom_bsdf)
+    : _ref(ref < 0.0 ? 0.0 : ref) {
+    if (custom_bsdf) {
+        _bsdf = custom_bsdf;
+    } else {
+        _bsdf = std::make_shared<TransparentBSDF>(tex, _ref);
+    }
+}
+
 } // namespace Raytracer
--- src/materials/transparent_material/TransparentMaterial.hpp
+++ src/materials/transparent_material/TransparentMaterial.hpp
@@ -14,14 +14,7 @@ class TransparentMaterial : public IMaterial {
 public:
     TransparentMaterial(std::shared_ptr<ITexture> tex,
                         double ref = 0.0,
-                        std::shared_ptr<IBSDF> custom_bsdf = nullptr)
-        : _ref(ref < 0.0 ? 0.0 : ref) {
-        if (custom_bsdf) {
-            _bsdf = custom_bsdf;
-        } else {
-            _bsdf = std::make_shared<TransparentBSDF>(tex, _ref);
-        }
-    }
+                        std::shared_ptr<IBSDF> custom_bsdf = nullptr);
 
     const IBSDF& getBSDF() const override { return *_bsdf; }
 
