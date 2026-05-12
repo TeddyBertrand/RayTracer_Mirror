8d214955b3f916685d7f96cec507de34679e6959
maeva <maeva.arroniz@epitech.eu>
2026-05-12 17:46:35 +0200

    feat(normal mapping): move get_perturbed_normal method for normal mapping calculations

--- src/materials/commun/bsdf/normal_mapping/NormalMappingBSDF.hpp
+++ src/materials/commun/bsdf/normal_mapping/NormalMappingBSDF.hpp
@@ -28,24 +28,7 @@ private:
     std::shared_ptr<ITexture> _normal_map;
     double _strength;
 
-    Vector3D get_perturbed_normal(const HitRecord& hit) const {
-
-        Color n_color = _normal_map->value(hit.u, hit.v, hit.point);
-        Vector3D local_n(2.0 * n_color.r - 1.0, 2.0 * n_color.g - 1.0, 2.0 * n_color.b - 1.0);
-
-        Vector3D up = std::fabs(hit.normal.y) < 0.999 ? Vector3D(0, 1, 0) : Vector3D(1, 0, 0);
-        Vector3D tangent = up.cross(hit.normal);
-        if (tangent.lengthSquared() < 1e-12) {
-            tangent = Vector3D(1, 0, 0).cross(hit.normal);
-        }
-        tangent = (tangent - hit.normal * hit.normal.dot(tangent)).normalized();
-        Vector3D bitangent = hit.normal.cross(tangent).normalized();
-
-        Vector3D perturbed =
-            (tangent * local_n.x + bitangent * local_n.y + hit.normal * local_n.z).normalized();
-
-        return (hit.normal * (1.0 - _strength) + perturbed * _strength).normalized();
-    }
+    Vector3D get_perturbed_normal(const HitRecord& hit) const;
 };
 
 } // namespace Raytracer
