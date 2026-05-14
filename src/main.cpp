fb920949fd7c85804fabe98a222add5a7c67f490
Amayyas <amayyas.aouadene@epitech.eu>
2026-05-14 20:54:04 +0200

    feat(network): add worker mode CLI arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include "core/Raytracer.hpp"
+#include "core/network/Cluster.hpp"
+#include <string>
 
 int main(int argc, const char* argv[]) {
-    Raytracer::Raytracer raytracer(argc, argv);
+    if (argc >= 3 && std::string(argv[1]) == "--worker") {
+        unsigned short port = std::stoi(argv[2]);
+        Raytracer::ClusterWorker worker(port);
+        worker.run();
+        return 0;
+    }
 
+    Raytracer::Raytracer raytracer(argc, argv);
     raytracer.run();
     return raytracer.getStatus();
 }
