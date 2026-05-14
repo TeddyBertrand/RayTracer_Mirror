dda3ca6bcc614c5815c6a8dfccc932139c3b5e7f
Amayyas <amayyas.aouadene@epitech.eu>
2026-05-14 20:54:42 +0200

    feat(network): add Cluster class definitions

---
+++ src/core/network/Cluster.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "core/scene/Scene.hpp"
+#include "render/FrameBuffer.hpp"
+#include <SFML/Network.hpp>
+#include <memory>
+#include <string>
+#include <thread>
+#include <vector>
+
+namespace Raytracer {
+
+// Skeleton for the Worker
+class ClusterWorker {
+public:
+    ClusterWorker(unsigned short port);
+    void run();
+
+private:
+    unsigned short _port;
+    sf::TcpListener _listener;
+    bool _running = true;
+};
+
+// Skeleton for the Master's Network Renderer
+class ClusterClient {
+public:
+    ClusterClient(const std::vector<std::string>& workerAddresses, unsigned short port);
+
+    // Divide and send work
+    void distributeRender(const std::string& scenePath,
+                          FrameBuffer& targetBuffer,
+                          int width,
+                          int height);
+
+private:
+    std::vector<std::string> _workerAddresses;
+    unsigned short _port;
+    std::vector<std::unique_ptr<sf::TcpSocket>> _sockets;
+};
+
+} // namespace Raytracer
